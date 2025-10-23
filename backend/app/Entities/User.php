d3ba82700dbe76b33516ef3c2bbd080250e08463
Yono <terrenceespiritu21@gmail.com>
2025-10-23 12:48:56 +0800

    -updated Entities/User.php

--- backend/app/Entities/User.php
+++ backend/app/Entities/User.php
@@ -6,14 +6,31 @@ use CodeIgniter\Entity\Entity;
 
 class User extends Entity
 {
+    // Optional field remapping (useful if you want to alias names later)
+    protected $datamap = [];
+
+    // The fields that should be automatically converted to DateTime objects
     protected $dates = ['created_at', 'updated_at', 'deleted_at'];
 
+    // Define how certain fields should be automatically type-cast
+    protected $casts = [
+        'id'              => 'integer',
+        'account_status'  => 'integer',
+        'email_activated' => 'integer',
+    ];
+
+    /**
+     * Helper method to get the full name of the user.
+     */
     public function getFullName(): string
     {
         return trim("{$this->first_name} {$this->middle_name} {$this->last_name}");
     }
 
-    public function setPassword(string $password)
+    /**
+     * Setter for password to ensure it's hashed before being stored.
+     */
+    public function setPassword(string $password): self
     {
         $this->attributes['password_hash'] = password_hash($password, PASSWORD_DEFAULT);
         return $this;
